a76ad98bed9aec9431440764516808471c359eff
DStromberg <110654302+stromberg-DS@users.noreply.github.com>
2024-06-07 14:46:34 -0600

    Delete Particle/Adafruit_Publish_Subscribe/src/credentials.h

--- Particle/Adafruit_Publish_Subscribe/src/credentials.h
+++
@@ -1,4 +0,0 @@
-#define AIO_SERVER "io.adafruit.com"
-#define AIO_SERVERPORT 1883             //use 1883 for SSL
-#define AIO_USERNAME "explora_exhibits"
-#define AIO_KEY "aio_ryCh41BTAZJG8CmNduf5JDEbleno"
