77969d615e1f12e0224a83f41e5cf3cdfab89154
DStromberg <110654302+stromberg-DS@users.noreply.github.com>
2024-10-12 01:40:38 -0600

    Cleaned up for comparing 2 colors to each other.

--- Particle/NeoPix_ColorPicker/src/NeoPix_ColorPicker.cpp
+++ Particle/NeoPix_ColorPicker/src/NeoPix_ColorPicker.cpp
@@ -11,12 +11,12 @@
 SYSTEM_MODE(AUTOMATIC);
 SYSTEM_THREAD(ENABLED);
 
-const int PIXEL_COUNT = 2000;
+const int PIXEL_COUNT = 16;
 const int BUTTON_PIN = D10;
 const int RED_POT_PIN = A5;
 const int GREEN_POT_PIN = A0;
 const int BLUE_POT_PIN = A1;
-const int MAX_BRIGHTNESS = 150;
+const int MAX_BRIGHTNESS = 85;
 
 int colorSplit1 = 40;
 int colorSplit2 = 100;
@@ -24,22 +24,25 @@ int redVal = 0;
 int greenVal = 0;
 int blueVal = 0;
 int rgbVal = 0;
+bool isUsingOnePot = false;   //choose if using one pot and button or 3 pots.
+
+int baseColor = 0x991100;     //base color to test against.
 
 int potMode = 0;
 int currentColor = 0;
 
 unsigned int currentMillis;
-int lastMillis = -9999;
+int lastMillis = -9999; 
 
 Adafruit_NeoPixel pixel(PIXEL_COUNT, SPI1, WS2812);
 Button modeSwitch(BUTTON_PIN);
 
 void setup() {
-  // Serial.begin(9600);
-  // while(!Serial.isConnected()){
-  //   //wait....
-  // }
-  // Serial.printf("Serial Connected!\n\n");
+  Serial.begin(9600);
+  while(!Serial.isConnected()){
+    //wait....
+  }
+  Serial.printf("Serial Connected!\n\n");
   pixel.begin();
   pixel.setBrightness(MAX_BRIGHTNESS);
 
@@ -64,12 +67,25 @@ void loop() {
 
   // pixel.clear();
 
-  redPotReading = analogRead(RED_POT_PIN);
-  // redVal = map(redPotReading, 0, 4096, 0, 255);
-  greenPotReading = analogRead(GREEN_POT_PIN);
-  // greenVal = map(greenPotReading, 0, 4096, 0, 255);
-  bluePotReading = analogRead(BLUE_POT_PIN);
-  // blueVal = map(bluePotReading, 0, 4096, 0, 255);
+  if(isUsingOnePot){
+    // if there is only one pot
+    if(currentColor == 0){
+      redVal = map(redPotReading, 0, 4096, 0, 255);
+    }else if (currentColor == 1){
+      greenVal = map(redPotReading, 0, 4096, 0, 255);
+    } else{
+      blueVal = map(redPotReading, 0, 4096, 0, 255);
+    }
+  }else{
+    redPotReading = analogRead(RED_POT_PIN);
+    redVal = map(redPotReading, 0, 4096, 0, 255);
+    greenPotReading = analogRead(GREEN_POT_PIN);
+    greenVal = map(greenPotReading, 0, 4096, 0, 255);
+    bluePotReading = analogRead(BLUE_POT_PIN);
+    blueVal = map(bluePotReading, 0, 4096, 0, 255);
+  }
+
+
 
   if(modeSwitch.isClicked()){
     currentColor++;
@@ -80,25 +96,22 @@ void loop() {
     delay(50);  //delay for a bit of debouncing.
   }
 
-// if there is only one pot
-  if(currentColor == 0){
-    redVal = map(redPotReading, 0, 4096, 0, 255);
-  }else if (currentColor == 1){
-    greenVal = map(redPotReading, 0, 4096, 0, 255);
-  } else{
-    blueVal = map(redPotReading, 0, 4096, 0, 255);
-  }
+
 
   rgbVal = (redVal<<16) | (greenVal<<8) | blueVal;
   
-  for(int i=0; i<PIXEL_COUNT; i++){
+  for(int i=0; i<PIXEL_COUNT/2; i++){
     pixel.setPixelColor(i, rgbVal);
+  }
+
+  for(int j=PIXEL_COUNT/2; j<PIXEL_COUNT; j++){
+    pixel.setPixelColor(j, baseColor);
   }
   pixel.show();
 
 
   if(timeSincePrint > 1000){  
-    // Serial.printf("rgbVal: %06X\n\n", rgbVal);
+    Serial.printf("rgbVal: %06X\n\n", rgbVal);
     lastMillis = currentMillis;
   }
 }
